4bd08477b459dc4a29172368fbf312d359fc93d5
saikumartanakala <119582244+saikumartanakala@users.noreply.github.com>
2025-07-20 22:55:27 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  document.getElementById('form-response').innerText = "Thank you! We’ll get back to you soon.";
-  this.reset();
-});
